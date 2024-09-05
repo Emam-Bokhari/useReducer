9e5564392dc31d003edc03e8c993dc8575b375d0
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-05 10:43:03 +0600

    feat(counter): implemented useReducer hook for increment and decrement functionalit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+import Counter from "./components/Counter";
+
+export default function App() {
+  return (
+    <Fragment>
+      <Counter />
+    </Fragment>
+  );
+}
